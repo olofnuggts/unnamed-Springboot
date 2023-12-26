e48288022eeb17d0fe1cc4c1cac8234e8bda120a
olofnuggets <yousef_kanzouh@yahoo.com>
2023-12-26 09:47:47 +0100

    adding refresh Token

--- src/main/java/com/yk/unnamed/service/AuthenticationService.java
+++ src/main/java/com/yk/unnamed/service/AuthenticationService.java
@@ -4,7 +4,10 @@ import com.yk.unnamed.controller.AuthenticationRequest;
 import com.yk.unnamed.controller.AuthenticationResponse;
 import com.yk.unnamed.controller.RegisterRequest;
 import com.yk.unnamed.model.Role;
+import com.yk.unnamed.model.Token;
+import com.yk.unnamed.model.TokenType;
 import com.yk.unnamed.model.User;
+import com.yk.unnamed.repository.TokenRepository;
 import com.yk.unnamed.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,6 +19,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthenticationService {
     private final UserRepository repository;
+    private final TokenRepository tokenRepository;
     private final PasswordEncoder  passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager    authenticationManager;
@@ -28,16 +32,42 @@ public AuthenticationResponse register (RegisterRequest request){
             .role(Role.USER)
             .build();
     repository.save(user);
+    var savedUser = repository.save(user);
     var jwtToken  = jwtService.generateToken(user);
+    var refreshToken = jwtService.generateRefreshToken(user);
+    saveUserToken(savedUser, jwtToken);
 
-    return AuthenticationResponse.builder().token(jwtToken).build();
+    return AuthenticationResponse.builder().accessToken(jwtToken).refreshToken(refreshToken).build();
 }
     public AuthenticationResponse authenticate (AuthenticationRequest request){
     authenticationManager.authenticate( new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword()));
     var user = repository.findByEmail(request.getEmail()).orElseThrow();
 
         var jwtToken  = jwtService.generateToken(user);
+        var refreshToken = jwtService.generateRefreshToken(user);
+        revokeAllUserTokens(user);
 
-        return AuthenticationResponse.builder().token(jwtToken).build();
+        return AuthenticationResponse.builder().accessToken(jwtToken).refreshToken(refreshToken).build();
     }
+    private void saveUserToken(User user, String jwtToken) {
+        var token = Token.builder()
+                .user(user)
+                .token(jwtToken)
+                .tokenType(TokenType.BEARER)
+                .expired(false)
+                .revoked(false)
+                .build();
+        tokenRepository.save(token);
+    }
+    private void revokeAllUserTokens(User user) {
+        var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
+        if (validUserTokens.isEmpty())
+            return;
+        validUserTokens.forEach(token -> {
+            token.setExpired(true);
+            token.setRevoked(true);
+        });
+        tokenRepository.saveAll(validUserTokens);
+    }
+
 }
